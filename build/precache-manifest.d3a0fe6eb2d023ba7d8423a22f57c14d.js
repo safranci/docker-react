f9a276eaf9f19c77ce0e9a1295f1c3fb5f4ed745
sandhya <sandhya.francis@hpe.com>
2020-02-03 13:48:52 -0800

    Adding build folder back

---
+++ build/precache-manifest.d3a0fe6eb2d023ba7d8423a22f57c14d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b61e36e7557327240a79edafc5d448d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2764e08a27e686e97d53",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "163dd15cca523a63d899",
+    "url": "/static/js/2.970da5d1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.970da5d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2764e08a27e686e97d53",
+    "url": "/static/js/main.633ca20a.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
